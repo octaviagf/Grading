095eca9e60b2d083514f826c10eec2623d5312fa
Octavia Garcia <oagf0616@gmail.com>
2023-09-10 20:18:46 -0400

    Uploading files

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const showGrade = document.getElementById("showgrade");
+showGrade.addEventListener("click", grading);
+
+function grading() {
+
+    let grade = document.getElementById("grade").value;
+
+    if (grade >= 90 && grade == 100) {
+        Swal.fire({
+            icon: "success",
+            title: "A",
+            text: "You passed!"
+        })
+    } else if (grade >= 80 && grade <= 89) {
+        Swal.fire({
+            icon: "success",
+            title: "B",
+            text: "You passed!"
+        })
+    } else if (grade >= 70 && grade <=79) {
+        Swal.fire({
+            icon: "success",
+            title: "C",
+            text: "You passed!"
+        })
+    } else if (grade >=60 && grade <= 69) {
+        Swal.fire({
+            icon: "warning",
+            title: "D",
+            text: "You falied :("
+        })
+    } else if (grade <= 59){
+        Swal.fire({
+            icon: "warning",
+            title: "F",
+            text: "You failed :'("
+        })
+    } else {
+        Swal.fire({
+            icon: "error",
+            text: "Your grade is invalid."
+        })
+    }
+}
